8627bbeb737bcc186063e7a08f44f4fae2c507be
Aranchai Moonkum <aranchai@Aranchais-MacBook-Pro.local>
2025-03-06 22:38:38 +0700

    created a header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,13 @@
 export default function Home() {
     return (
-        <></>
+        <main>
+            <div className="flex items-center justify-center mx-auto max-w-1/2 min-h-dvh">
+                {/* Header */}
+                <header className="flex flex-col items-center justify-center space-y-2">
+                    <h1 className="text-5xl font-bold">Weroniczka</h1>
+                    <p className="text-xl tracking-wide">Content Creator & K-Beauty Lover</p>
+                </header>
+            </div>
+        </main>
     )
 }
